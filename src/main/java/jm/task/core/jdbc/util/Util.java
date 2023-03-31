720291a272a32f93d99efd9c0a5c62285e05cbd9
volRender <litvishko.render@gmail.com>
2023-03-31 15:33:48 +0300

    Create connection with Hibernate components in Util class.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,10 +1,17 @@
 package jm.task.core.jdbc.util;
 
+import jm.task.core.jdbc.model.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class Util {
+    // configuration using JDBC
     private static final String DB_DRIVER = "com.mysql.cj.jdbc.Driver";
     private static final String DB_URL = "jdbc:mysql://localhost:3306/my_db?useSSL=false&serverTimezone=UTC";
     private static final String DB_USERNAME = "root";
@@ -21,4 +28,25 @@ public class Util {
         }
         return connection;
     }
+
+    // configuration using Hibernate
+    private static SessionFactory buildSessionFactory() {
+        try {
+            Properties prop = new Properties();
+            prop.put(Environment.DRIVER, DB_DRIVER);
+            prop.put(Environment.URL, DB_URL);
+            prop.put(Environment.USER, DB_USERNAME);
+            prop.put(Environment.PASS, DB_PASSWORD);
+            return new Configuration()
+                    .setProperties(prop)
+                    .addAnnotatedClass(User.class)
+                    .buildSessionFactory();
+        } catch (Throwable e) {
+            e.printStackTrace();
+            System.out.println("Ошибка соединения");
+        }
+        return null;
+    }
 }
+
+
